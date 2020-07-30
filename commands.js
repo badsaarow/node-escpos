6630930c74bfe17c0c8c3780d31cdcd0285ef7e9
sungyong <sungyong@gmail.com>
2020-07-30 13:34:15 +0900

    fix sonar warnging
    Expected an assignment or function call and instead saw an expression.

--- commands.js
+++ commands.js
@@ -299,7 +299,7 @@ _.GSV0_FORMAT = {
  * [BEEP description]
  * @type {string}
  */
-_.BEEP = '\x1b\x42', // Printer Buzzer pre hex
+_.BEEP = '\x1b\x42'; // Printer Buzzer pre hex
   
 /**
  * [COLOR description]
